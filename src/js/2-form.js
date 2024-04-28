1c6b77b4d7efc35df9c76feecb0fd55e2872ee1d
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-04-28 11:50:33 +0300

    JavaSC

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,21 +1,12 @@
-const formData = {
- email: "",
- message: "",
-}
+const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || { email: "", message: "" };
 
-const form = document.querySelector(".feedback-form");
-const formStringData = localStorage.getItem("feedback-form-state") ?? "";
-const formParseData = formStringData ? JSON.parse(formStringData) : { email: "", message: "" };
-form.elements.email.value = formParseData.email;
-form.elements.message.value = formParseData.message;
-form.addEventListener("input", () => {
-    formData.email = form.elements.email.value.trim()
-    formData.message = form.elements.message.value.trim()
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
-    
-})
+form.addEventListener("change", () => {
+    formData.email = form.elements.email.value.trim();
+    formData.message = form.elements.message.value.trim();
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
 form.addEventListener("submit", evt => {
-    evt.preventDefault()
+    evt.preventDefault();
     const submitFormData = {
         email: evt.target.elements.email.value.trim(),
         message: evt.target.elements.message.value.trim()
@@ -25,7 +16,7 @@ form.addEventListener("submit", evt => {
         return alert("All form fields must be filled in");
     }
     
-    console.log(submitFormData)
-    localStorage.removeItem("feedback-form-state")
-    form.reset()
-})
+    console.log(submitFormData);
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+});
