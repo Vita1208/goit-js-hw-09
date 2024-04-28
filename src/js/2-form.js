2a251d3ef9997d26cf974b45d99783c606cb402b
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-04-28 14:21:15 +0300

    java

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,28 +1,31 @@
-const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || { email: "", message: "" };
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector(".feedback-form");
+    const emailInput = form.querySelector("input[name='email']");
 
-const form = document.getElementById('feedback-form');
+    emailInput.addEventListener('change', updateFormData);
 
-form.elements.email.addEventListener('change', updateFormData);
-form.elements.message.addEventListener('change', updateFormData);
-
-function updateFormData() {
-    formData.email = form.elements.email.value.trim();
-    formData.message = form.elements.message.value.trim();
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-}
-
-form.addEventListener("submit", evt => {
-    evt.preventDefault();
-    const submitFormData = {
-        email: evt.target.elements.email.value.trim(),
-        message: evt.target.elements.message.value.trim()
-    };
-    
-    if (submitFormData.email === "" || submitFormData.message === "") {
-        return alert("All form fields must be filled in");
+    function updateFormData() {
+        const formData = {
+            email: emailInput.value.trim(),
+            message: form.querySelector("textarea[name='message']").value.trim()
+        };
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
     }
-    
-    console.log(submitFormData);
-    localStorage.removeItem("feedback-form-state");
-    form.reset();
+
+    form.addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        const submitFormData = {
+            email: emailInput.value.trim(),
+            message: form.querySelector("textarea[name='message']").value.trim()
+        };
+        
+        if (submitFormData.email === "" || submitFormData.message === "") {
+            return alert("All form fields must be filled in");
+        }
+        
+        console.log(submitFormData);
+        localStorage.removeItem("feedback-form-state");
+        form.reset();
+    });
 });
+
